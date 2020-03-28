bc683bb70d93706c0d37022359e358cd8ad8c0d9
Haizul <proxy.shadowing@gmail.com>
2020-03-28 15:58:41 +0800

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create(
+    title: "My blog post no #{blog}",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati ab provident expedita quisquam minus. Facilis cupiditate obcaecati ipsa esse, optio excepturi sint nemo corrupti voluptatum exercitationem eos, recusandae fugiat molestias?"
+  )
+end
+puts  "10 blogs created"
+
+  5.times do |skill|
+    Skill.create(
+      title:"My Skill #{skill}",
+      percent_utilized:"100"
+    )
+  end
+  puts "5 skills created"
+
+  9.times do |portfolio|
+    Portfolio.create(
+      title:"Porfolio #{portfolio}",
+      subtitle:"Subtitle #{portfolio}",
+      body:"Obcaecati ab provident expedita quisquam minus. Facilis cupiditate obcaecati ipsa esse, optio excepturi sint nemo corrupti voluptatum exercitationem eos, recusandae fugiat molestias?",
+      main_image:"https://images.unsplash.com/photo-1558980664-2506fca6bfc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+      thumb_image:"https://images.unsplash.com/photo-1584118624012-df056829fbd0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
+
+    )
+  end
+  puts  "9 portfolios created"
+
