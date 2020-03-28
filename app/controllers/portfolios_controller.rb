78922072f8b4cab131f8feb19399d99e1b2f36dc
Haizul <proxy.shadowing@gmail.com>
2020-03-28 17:23:42 +0800

    integrated new and create action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,17 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image ))
+    if @portfolio_item.save
+      redirect_to portfolios_path
+    else
+      render 'new'
+    end
+  end
 end
